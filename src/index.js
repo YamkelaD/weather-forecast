ff7fbaf369f0e925b7aaceb4f6a957404f5dc381
YamkelaD <117226069+YamkelaD@users.noreply.github.com>
2025-02-24 11:16:58 +0200

    added more weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,41 @@ function refreshWeather(response) {
   let tempElement = document.querySelector("#temperature-value");
   let temp = response.data.temperature.current;
   let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let time = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  console.log(response.data);
 
   city.innerHTML = response.data.city;
+  time.innerHTML = formatDate(date);
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
   tempElement.innerHTML = Math.round(temp);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
